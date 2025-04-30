de1b4536d9a67f016ce10895115d6fc93c5f603a
Mayur Sarma <2725360@tcs.com>
2025-04-30 12:22:45 +0530

    database + connection

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,3 +1,24 @@
-import { migrate } from "drizzle-orm/neon-serverless/migrator";
+import { migrate } from "drizzle-orm/neon-http/migrator";
 import { drizzle } from "drizzle-orm/neon-http";
 import {neon} from "@neondatabase/serverless"
+
+import * as dotenv from "dotenv" ;
+
+dotenv.config();
+if(!process.env.DATABASE_URL) {
+    throw new Error("Database url is not set in .env")
+}
+
+async function runMigration() {
+    try {
+        const sql = neon(process.env.DATABASE_URL!) ;
+        const db = drizzle(sql) //initialize the connection
+        await migrate(db,{migrationsFolder: "./drizzle"}) ;
+        console.log("All migration are successfully done") ;
+    } catch (error: any) {
+        console.log(error.message);
+        process.exit(1) ;
+    }
+}
+
+runMigration() ;
