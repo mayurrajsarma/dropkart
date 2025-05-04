b1c4fb866cd2ca232a4b5c6dd0750567a2582cc6
mayurrajsarma <mayurrajsarma202@gmail.com>
2025-05-04 11:19:41 +0530

    zod schema

--- schemas/signInSchema.ts
+++ schemas/signInSchema.ts
@@ -9,12 +9,5 @@ export const signupSchema = z.object({
         .string()
         .min(1,{message: "Password is required"})
         .min(8,{message: "Password should be minimum of 8 characters"}),
-    passwordConfirmation: z
-        .string()
-        .min(1,{message: "Please confirm your password"}),
-})
-
-.refine((data)=> data.password === data.passwordConfirmation, {
-    message: "Password do not match",
-    path: ["passwordConfirmation"] //the error message will go to passwordConfirmation path
+    
 })
